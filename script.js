c8c04581f1fbb31539e6376210983de7f682492a
Carmen <path4design@gmail.com>
2023-05-11 14:21:14 +0200

    challenge complete 🥳

--- script.js
+++ script.js
@@ -9,78 +9,81 @@ const form = document.querySelector("form")
 const mapField = document.querySelector("#map")
 const inputBtn = document.querySelector("button")
 
-
-form.addEventListener('submit', (event) =>{
-    event.preventDefault()
-    inputBtn.addEventListener('click', () => {
-        let ip = inputField.value
-    console.log("Me has dado click: ", ip)
-    getLocationfromApi(ip);
-})
-})
-// Lograr que el submit nos arroje el valor al clicar el boton
-
-
-
-async function getLocationfromApi(location){
+async function getLocationfromApi(location) {
 
     const url = `https://ipapi.co/${location}/json/`
     console.log("URL: ", url)
 
     fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        ipField.textContent = data.ip
-        console.log(ipField)
-        locationField.textContent = `${data.city}, ${data.region}, ${data.country}`
-        console.log(locationField)
-        const timeZone = data.utc_offset
-        timeZoneField.textContent = `UTC: ${timeZone}`
-        console.log(timeZoneField)
-        ispField.textContent = data.org
-        console.log(ispField)
-        const latitude = data.latitude
-        const longitude = data.longitude
-        console.log(latitude, longitude)
-        var map = L.map('map').setView([latitude, longitude], 13);
-        console.log(map)
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-        var marker = L.marker([latitude, longitude]).addTo(map);
-        var circle = L.circle([latitude, longitude], {
-            color: 'red',
-            fillColor: '#f03',
-            fillOpacity: 0.5,
-            radius: 500
-        }).addTo(map);
-        var polygon = L.polygon([
-            [latitude, longitude],
-            [latitude, longitude],
-            [latitude, longitude] 
-        ]).addTo(map);
-        marker.bindPopup("ip").openPopup(); 
-        circle.bindPopup("I am a circle.");
-        polygon.bindPopup("I am a polygon.");
-        var popup = L.popup()
+        .then(response => response.json())
+        .then(data => {
+            ip = data.ip
+            ipField.textContent = ip
+            console.log(ipField)
+            locationField.textContent = `${data.city}, ${data.region}, ${data.country}`
+            console.log(locationField)
+            const timeZone = data.utc_offset
+            timeZoneField.textContent = `UTC: ${timeZone}`
+            console.log(timeZoneField)
+            ispField.textContent = data.org
+            console.log(ispField)
+            const latitude = data.latitude
+            const longitude = data.longitude
+            console.log(latitude, longitude)
+
+            generateMap(latitude, longitude)
+
+        })
+}
+
+function generateMap(latitude, longitude) {
+    var map = L.map('map').setView([latitude, longitude], 13);
+    console.log(map)
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    var marker = L.marker([latitude, longitude]).addTo(map);
+    var circle = L.circle([latitude, longitude], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+    }).addTo(map);
+    var polygon = L.polygon([
+        [latitude, longitude],
+        [latitude, longitude],
+        [latitude, longitude]
+    ]).addTo(map);
+    marker.bindPopup(ip).openPopup();
+    circle.bindPopup("I am a circle.");
+    polygon.bindPopup("I am a polygon.");
+    var popup = L.popup()
         .setLatLng([latitude, longitude])
         .setContent(ip)
         .openOn(map);
-        
-        var popup = L.popup();
 
-        function onMapClick(e) {
+    var popup = L.popup();
+
+    function onMapClick(e) {
         popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-        }
-
-        map.on('click', onMapClick);
-        })
-
+            .setLatLng(e.latlng)
+            .setContent("You clicked the map at " + e.latlng.toString())
+            .openOn(map);
     }
 
-// getLocationfromApi(location)
+    map.on('click', onMapClick);
+}
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    console.log("submit")
+    let ip = inputField.value
+    console.log("Me has dado click: ", ip)
+    getLocationfromApi(ip);
+
+})
+
+
+
 
